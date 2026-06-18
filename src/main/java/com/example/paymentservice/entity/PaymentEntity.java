6671924a270a92821ce76f64ba7c74aac8da2224
victoria <vasharchuk07@gmail.com>
2026-06-18 15:46:08 +0300

    feat: add db and entity

---
+++ src/main/java/com/example/paymentservice/entity/PaymentEntity.java
@@ -0,0 +1,34 @@
+package com.example.paymentservice.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Getter
+@Setter
+@Document(collection = "payments")
+public class PaymentEntity {
+
+    @Id
+    private String id;
+
+    @Field("order_id")
+    private String orderId;
+
+    @Field("user_id")
+    private Long userId;
+
+    @Field("status")
+    private String status;
+
+    @Field("timestamp")
+    private Instant timestamp;
+
+    @Field("payment_amount")
+    private BigDecimal paymentAmount;
+}
